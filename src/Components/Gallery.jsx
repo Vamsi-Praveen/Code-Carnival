36a782d241a18a95910c69d4ac363073d7e4a1e6
Vamsi-Praveen <21A91A05G5@aec.edu.in>
2023-10-08 18:00:32 +0530

    gallery done

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState ,useEffect} from 'react'
 import '../styles/gallery.css'
 import Navbar from './Navbar'
+import image from '../assets/about-1.jpg'
+import image1 from '../assets/about-2.jpg'
+import image2 from '../assets/about.jpg'
+import { Carousel } from './Carousel'
+import axios from "axios"
 
 
 const Gallery = () => {
+
+  const [data,setData] = useState()
+  async function get_gallery() {
+    await axios.get("http://localhost:8000/gallery/all")
+      .then((res) => {
+        setData(res.data)
+      })
+  }
+  useEffect(() => {
+    get_gallery();
+  }, [1])
   return (
     <>
       <Navbar color={"#fff"}/>
       <div className='gallery'>
-          <h1>Gallery</h1>
+        <Carousel data={data}/>
       </div>
     </>
   )
